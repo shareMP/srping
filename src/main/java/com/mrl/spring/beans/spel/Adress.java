a1de16640d374b7faa36b3b5cf22625131e67aaa
lwq <544520418@qq.com>
2017-12-27 11:23:23 +0800

    spel表达式

---
+++ src/main/java/com/mrl/spring/beans/spel/Adress.java
@@ -0,0 +1,22 @@
+package com.mrl.spring.beans.spel;
+
+public class Adress {
+	private String city;
+	private String street;
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	@Override
+	public String toString() {
+		return "Adress [city=" + city + ", street=" + street + "]";
+	}
+}
