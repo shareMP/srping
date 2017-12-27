98550609df2ac560674e646a1358c054a01a92ce
lwq <544520418@qq.com>
2017-12-27 23:09:51 +0800

    环绕通知

--- src/main/java/com/mrl/spring/aop/impl/LoggingAspect.java
+++ src/main/java/com/mrl/spring/aop/impl/LoggingAspect.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -19,7 +23,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect {
 	
-	//声明前置通知
+	/*//声明前置通知
 	@Before(value = "execution(public int com.mrl.spring.aop.impl.ArithmeticCalculator.*(int,int))")
 	public void beforeMethod(JoinPoint joinPoint){
 		String methodName = joinPoint.getSignature().getName();
@@ -34,4 +38,53 @@ public class LoggingAspect {
 		String methodName = point.getSignature().getName();
 		System.out.println("the method "+methodName+" end with ");
 	}
+	
+	//返回通知,在方法正常结束后执行的代码
+	//返回通知是可以访问到方法的返回值的
+	//有异常不会返回
+	@AfterReturning(value="execution(public int com.mrl.spring.aop.impl.ArithmeticCalculator.*(int,int))",returning="result")
+	public void afterReturning(JoinPoint joinPoint,Object result){
+		String methodName = joinPoint.getSignature().getName();
+		System.out.println("the method "+methodName+" ends with "+result);
+	}
+	
+	//异常通知
+	//在目标方法出现异常时，会执行的代码
+	//可以访问到异常对象，可以指定在出现特定异常时执行.
+	@AfterThrowing(value="execution(public int com.mrl.spring.aop.impl.ArithmeticCalculator.*(int,int))",throwing="ex")
+	public void afterThrowing(JoinPoint joinPoint,Exception ex){
+		String methodName = joinPoint.getSignature().getName();
+		System.out.println("the method "+methodName+" occcurs "+ex.toString());
+	}
+	*/
+	//环绕通知
+	//环绕通知需要携带 ProceedingJoinPoint餐参数 
+	//环绕通知类似于动态代理的全过程
+	//ProceedingJoinPoint 可以决定是否执行目标方法
+	//环绕通知必须有返回值，返回值即为目标方法的返回值
+	@Around("execution(public int com.mrl.spring.aop.impl.ArithmeticCalculator.*(int,int))")
+	public Object roundMessage(ProceedingJoinPoint point){
+		
+		Object res = null;
+		String methdName = point.getSignature().getName();
+		
+		//执行目标方法
+		try {
+			//前置通知
+			System.out.println("the method "+methdName+" begins with "+Arrays.asList(point.getArgs()));
+			res = point.proceed();
+			//返回通知
+			System.out.println("the method "+methdName+" ends with "+res);
+		} catch (Throwable e) {
+			System.out.println("the method "+methdName+" occcurs "+e.toString());
+			throw new RuntimeException(e);
+		}
+		
+		//后置通知
+		System.out.println("the method "+methdName+" ends");
+		
+		return  res;
+	}
+	
+	
 }
--- src/main/java/com/mrl/spring/aop/impl/Main.java
+++ src/main/java/com/mrl/spring/aop/impl/Main.java
@@ -8,11 +8,14 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("aop/applicationContext.xml");
 		ArithmeticCalculator arithmeticCalculator = (ArithmeticCalculator) ctx.getBean("arithmeticCalculator");
-		int res = arithmeticCalculator.add(1, 2);
+//		int res = arithmeticCalculator.add(1, 2);
+//		System.out.println(res);
 		
-		int res2 = arithmeticCalculator.div(4, 2);
+		int res2 = arithmeticCalculator.div(4, 0);
 		System.out.println(res2);
 		
+	/*	int res3 = arithmeticCalculator.mul(4, 2);
+		System.out.println(res3);*/
 		
 	}
 
