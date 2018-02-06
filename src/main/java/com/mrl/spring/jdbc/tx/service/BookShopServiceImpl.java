ca9fd1f39f549818a127640f2fc1c1608af38af4
lwq <544520418@qq.com>
2018-02-06 21:56:58 +0800

    1

--- src/main/java/com/mrl/spring/jdbc/tx/service/BookShopServiceImpl.java
+++ src/main/java/com/mrl/spring/jdbc/tx/service/BookShopServiceImpl.java
@@ -49,7 +49,6 @@ public class BookShopServiceImpl implements BookShopService {
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
